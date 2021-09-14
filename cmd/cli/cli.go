7135fc32d6e4b43b268d56d319a87b68dabeaa38
Antiselekotin <deksazon8@gmail.com>
2021-09-14 12:01:48 +0300

    cli client done

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	reader := bufio.NewReader(os.Stdin)
+	buf := []byte{}
+	isOpenQuote := false
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, c := range line {
+			if c == '"' {
+				isOpenQuote = !isOpenQuote
+			}
+		}
+		buf = append(buf, line...)
+		if !isOpenQuote {
+			isLineEnded, endedFrom := lineEnded(&buf)
+			if isLineEnded {
+				fmt.Printf("\n%s\n", buf[:endedFrom+1])
+				buf = []byte{}
+			}
+		}
+	}
 }
